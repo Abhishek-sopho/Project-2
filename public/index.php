607ea8c3973b9b3e92d688c2a8968754902f19d5
Abhishek-sopho <abhishek.kumar488@gmail.com>
2017-03-16 22:54:23 +0530

    register and login files

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>index.php</title>
+	</head>
+	<body>
+		<ul>
+			<li><a href="login.php">Click to Sign in</a></li>
+			<li><a href="store.php">Go to Store</a></li>
+		</ul>
+
+</html>
---
+++ public/login.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<body>
+		<form>
+			Email:<input type="text" name="email"><br>
+			Password:<input type="password" name="password"><br>
+			<button type="submit">Log In</button>,or<a href="register.html">Register</a>
+		</form>
+	</body>
+</html>
