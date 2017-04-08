c18461b5cb47bc7f91ee02f80b9343184c3e7f07
Abhishek-sopho <abhishek.kumar488@gmail.com>
2017-04-08 09:08:27 +0530

    dbconnect

---
+++ model/dbconn.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+$dbconnect = mysqli_connect('localhost', 'abhishekkumara23', '3vXt73bGW7mEcGnIg','project2');
+if (!$dbconnect) {
+    die('Could not connect: ' . mysql_error());
+}
+
+?>
