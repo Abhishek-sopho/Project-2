a3301c3044046128da165c12fe221692fea8a1b3
Abhishek-sopho <abhishek.kumar488@gmail.com>
2017-04-10 12:07:26 +0530

    changes made in userdb and logindb

--- model/logindb.php
+++ model/logindb.php
@@ -1,19 +1,49 @@
 <?php
 
 require "dbconn.php";
+session_start();
 
-$sql = "SELECT * FROM user WHERE name = $_POST['uname']";
-$result = $conn->query($sql);
+if(isset($_POST['submit'])){
+    $password =$_POST['psw'];
+    $name = $_POST['uname'];
+    //echo $password;
+    $sel = "select * from user where name = '$name' ";
+    $run = mysqli_query($dbconnect,$sel);
+    $row = mysqli_fetch_array($run);
+    $pass = $row['password'];
+    $id=$row['id'];
+    
+    $count = mysqli_num_rows($run);
+    if($password==$pass){
+        $_SESSION['id']=$id;
+        //echo "<script>alert('dfevgs')</script>";
+        echo "<script>window.open('../public/store.php','_self')</script>";
+    }
+    else{
+        echo "<script>alert('username or password incorrect!!!')</script>";
+        echo "<script>window.open('../public/login.php','_self')</script>";
+        //exit();
+    }
+}
+
+/*$name = $_POST["uname"];
+
+$sql = "SELECT * FROM user WHERE name = '$name'";
+//$result = $dbconnect->query($sql);
+$run=mysqli_query($dbconnect,$sql);
+$row = mysqli_fetch_array($run);
+$password = $row['password'];
+$count = mysqli_num_rows($run);
 
         // if we found user, check password
-        if (count($result) == 1)
+        if ($count == 1)
         {
             // first (and only) row
-            $row = $result[0];
+            //$row = $result[0];
             session_start();
 
             // compare hash of user's input against hash that's in database
-            if ($_POST["psw"] === $row["password"]))
+            if ($_POST["psw"] == $password))
             {
                 // remember that user's now logged in by storing user's ID in session
                 $_SESSION["id"] = $row["id"];
@@ -21,10 +51,15 @@ $result = $conn->query($sql);
                 // redirect to portfolio
                 header("Location: ../public/store.php");
             }
+            else
+            {
+                echo "sdf";
+            }
         }
+        else echo "werr";
 
 
-
+*/
 
 ?>
 
--- model/userdb.php
+++ model/userdb.php
@@ -2,25 +2,29 @@
 
 require "dbconn.php";
 
+$email = $_POST["email"];
 $name = $_POST["uname"];
 $pass = $_POST["psw"];
 $repass = $_POST["rpsw"];
 
-if($pass !== $repass)
+if($pass != $repass)
 {
-	<script>alert("Password do not match");</script>
-	exit;
+	<script>alert("Password do not match")</script>
+	header("Location: ../public/register.php");
+	
 }
 else
 {
 
 
-$sql = "INSERT INTO user (name,password) VALUES ('$name','$pass')";
-$result = $conn->query($sql);
+$sql = "INSERT INTO user (email,name,password) VALUES ('$email','$name','$pass')";
 
-echo "success";
+$run=mysqli_query($dbconnect,$sql);
 
 }
+
+
+
 
 
 header("Location: ../public/store.php");
